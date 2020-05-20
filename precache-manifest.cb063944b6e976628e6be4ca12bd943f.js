7cb5785a8b0f3279e0bf40784fe3b7c10004f543
Fareed Sokar <fareed.sokar@clicktale.com>
2020-05-20 19:44:51 +0300

    Updates

---
+++ precache-manifest.cb063944b6e976628e6be4ca12bd943f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a997f096fa63bc55253ad83bc0c28762",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "29b4c86d276e80519c25",
+    "url": "/tictactoe/static/css/main.8460444d.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/tictactoe/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b4c86d276e80519c25",
+    "url": "/tictactoe/static/js/main.b1448984.chunk.js"
+  },
+  {
+    "revision": "5f8a470de2451e71dd20",
+    "url": "/tictactoe/static/js/runtime-main.59855f66.js"
+  }
+]);
